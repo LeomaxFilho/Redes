477b4d3b473cfd2e0fa3da5ac0862557c10a5aae
Leomax <leomax.filho@gmail.com>
2024-10-25 21:11:50 -0300

    fix: returns

--- Projeto Final - Versao Local/requisidor.py
+++ Projeto Final - Versao Local/requisidor.py
@@ -1,11 +1,10 @@
 import random
-import sys
-import struct
+from scapy.all import *
 
 def gerar_requisicao(req_res, tipo, identificador):
     req_res = req_res & 0xF
     tipo = tipo & 0xF
-    mensagem = ((((identificador << 4) | req_res) << 4) | tipo) & 0xFFFFFF
+    mensagem = ((((identificador << 4) | req_res) << 4) |  tipo ) & 0xFFFFFF
     return mensagem
     
 def random_id(): 
@@ -13,15 +12,23 @@ def random_id():
 
 def receber_resposta(mensagem):
 
-    req_res = mensagem & 0xF
-    tipo = (mensagem >> 4) & 0xF
-    identificador = (mensagem >> 8) & 0xFFFF
-
     tamanho = (mensagem >> 24) & 0xFF
-    caractere = chr((mensagem >> 32) & 0xFF)
+    caractere = ''
 
     for i in range(tamanho):
-        caractere = chr((mensagem >> 32+(i*8)) & 0xFF)
-        print(caractere)
+        caractere += chr((mensagem >> 32+(i*8)) & 0xFF)
+    
+    print(caractere)
 
+def receber_resposta_int(mensagem):
 
+    caractere = (mensagem >> 32)
+
+    entra = caractere.to_bytes(4, byteorder='little')
+    sai = int.from_bytes(entra, byteorder='big')
+
+    print(sai)
+
+def process_packet(packet):
+    if packet.haslayer(UDP):
+        print("Pacote UDP recebido:", packet.summary())
--- Projeto Final - Versao Local/socket_udp.py
+++ Projeto Final - Versao Local/socket_udp.py
@@ -1,6 +1,5 @@
 import socket
 import requisidor
-import struct
 
 # Especificacoes do servidor 
 UDP_IP = "15.228.191.109"
@@ -10,92 +9,64 @@ UDP_PORT = 50000
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 while(True):   
-    op = int(input(print("[1] Data e Hora\n[2] Mensagem Motivacional\n[3] Respostas Emitidas\n[4] Sair: ")))
+    op = input("[1] Data e Hora\n[2] Mensagem Motivacional\n[3] Respostas Emitidas\n[4] Sair: ")
+    
+    # Gerar o identificador
+    identficador = requisidor.random_id()
 
-    # Data e hora
+    # * Data e hora
     if op == 1:
-        # Gera o identificador
-        identficador = requisidor.random_id()
+
         # Gera a mensagem para ser enviada para o servidor
         req = requisidor.gerar_requisicao(0, 0, identficador)
+
         # Conversao da mensagem de requisicao para um objeto de bytes
         msg = req.to_bytes(3, 'little')
+
         # Sock de enviando a mensagem para o servidor
         sock.sendto(msg, (UDP_IP, UDP_PORT))
+
         # Recebendo a mensagem do servidor
         msg_server, addr = sock.recvfrom(1024)
-        # Exibindo a mensagem em bytes do servidor e o endereco
-        print(f"Mensagem do Servidor: {msg_server}")
-        print(f"Endereco: {addr}")
-        # Mensagem em binario do servidor
-        representacao_binaria = ''.join(format(byte, '08b') for byte in msg_server)
-        print(representacao_binaria)    
+
         # Tipo de mensagem do servidor
-        requisidor.receber_resposta(int.from_bytes(msg_server, byteorder='little'))
+        palavra = requisidor.receber_resposta(int.from_bytes(msg_server, byteorder='little'))
 
-
-    # Mensagem motivacional
-    elif op == 2:   
-        # Gerar o Identificador
-        identficador = requisidor.random_id()
+    # * Mensagem motivacional
+    elif op == 2:
         # Gera a requisicao da mensagem
         req = requisidor.gerar_requisicao(0, 1, identficador)
 
-        print(bin(req))
-
         # Conversao da mensagem de requisicao para um objeto de bytes
         msg = req.to_bytes(3, 'little')
         
-        representacao_binaria = ''.join(format(byte, '08b') for byte in msg)
-        print(representacao_binaria)    
-        
         # Sock de enviando a mensagem para o servidor
         sock.sendto(msg, (UDP_IP, UDP_PORT))
+
         # Recebendo a mensagem do servidor
         msg_server, addr = sock.recvfrom(1024)
-        # Exibindo a mensagem em bytes do servidor e o endereco
-        """print(f"Mensagem do Servidor: {msg_server}")
-        print(f"Endereco: {addr}")"""
-        # Mensagem em binario do servidor
-        representacao_binaria = ''.join(format(byte, '08b') for byte in msg_server)
-        print(representacao_binaria)    
         
-        requisidor.receber_resposta(int.from_bytes(msg_server, byteorder='little'))
+        palavra = requisidor.receber_resposta(int.from_bytes(msg_server, byteorder='little'))
 
 
-    # Mensagem de quantidade de requisicoes
-    elif op == 3:  
-        # Gerar o identificador
-        identficador = requisidor.random_id()
+    # * Mensagem de quantidade de requisicoes
+    elif op == 3:
         # Resicao da mensagem
-        
         req = requisidor.gerar_requisicao(0, 2, identficador)
+        
         # Conversao da mensagem de requisicao para um objeto de bytes
-        print(bin(req))
         msg = req.to_bytes(3, 'little')    
 
-        representacao_binaria = ''.join(format(byte, '08b') for byte in msg)
-        print(representacao_binaria)       
-        
         # Sock de enviando a mensagem para o servidor
         sock.sendto(msg, (UDP_IP, UDP_PORT))
         
-        
         # Recebendo a mensagem do servidor
         msg_server, addr = sock.recvfrom(1024)
-        # Exibindo a mensagem em bytes do servidor e o endereco
-        print(f"Mensagem do Servidor: {msg_server}")
-        print(f"Endereco: {addr}")
-        # Mensagem em binario do servidor
-        representacao_binaria = ''.join(format(byte, '08b') for byte in msg_server)
-        print(representacao_binaria)    
+
         # Tipo de mensagem
-        requisidor.receber_resposta(int.from_bytes(msg_server, byteorder='little'))
+        palavra = requisidor.receber_resposta_int(int.from_bytes(msg_server, byteorder='little'))
 
     elif op == 4:   
         break
     else:
         print("OPCAO INVALIDA!!!")
-
-
-
